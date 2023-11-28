bc68c6114553b7aa58b8d576b0b76995129647a7
Gokula krishnan R <105733059+2002gokula@users.noreply.github.com>
2023-11-28 10:17:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <div>
+      <div className="bg-[#121212] hidden">
+        <div className=" flex">
+          <div className=" flex">
+            <div className=" flex ">
+              <span>1.3k</span>
+              <span>NFT Collections</span>
+            </div>
+            <div className=" flex ">
+              <span>0</span>
+              <span>Upcoming</span>
+            </div>
+            <div className="  flex ">
+              <span>1.3k</span>
+              <span>Influencers</span>
+            </div>
+            <div className="flex">
+              <span>30</span>
+              <span>Members</span>
+            </div>
+          </div>
+          <div className="">
+            <div className="">
+              <span>Join our discord!</span>
+              <a href="https://discord.gg/jpegarmy">
+                https://discord.gg/jpegarmy
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
